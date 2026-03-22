862a86b3db758c7663ef28d0494834636d2656a8
tushardacoder <tusharbasak071@gmail.com>
2026-03-22 12:44:56 +0600

    Notification system

---
+++ Notification_Service/Notification_System/NotificationService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notification_System
+{
+    public delegate void notify(string message);
+    public class NotificationService
+    {
+        public event notify Inapp;
+        public event notify Onsms;
+        public event notify Onemail;
+
+
+        public void sendInapp(string message)
+        {
+               Inapp?.Invoke(message);
+        }
+
+
+        public void sendsms(string message)
+        {
+            Onsms?.Invoke(message);
+        }
+
+        public void sendemdil(string message)
+        {
+
+            Onemail?.Invoke(message);
+        }
+    }
+}
---
+++ Notification_Service/Notification_System/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Notification Service System");
+
+Console.WriteLine("Press 1 -> Sign UP");
+Console.WriteLine("Press 2 -> Pay Bill");
+Console.WriteLine("Press 3 -> Order shipped");
+Console.WriteLine("Press 3 -> Any other key to sign out");
+
+
+while (true)
+{
+
+    var option = int.Parse(Console.ReadLine());
+
+    if (option == 1)
+    {
+
+
+    }
+
+    else if (option == 2)
+    {
+
+    }
+
+
+    else if (option == 3)
+    {
+
+    }
+
+    else
+    {
+        break;
+    }
+
+}
